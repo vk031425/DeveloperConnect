12d417303d6b1b4a23e598dd8ba8520bc3df6b33
Vinay_ <vinay989633@gmail.com>
2026-01-27 15:37:31 +0530

    ad

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -60,7 +60,7 @@ const Profile = () => {
     try {
       await api.post("/messages/send", {
         receiverId: userData._id,
-        text: "👋 Hey!",
+        text: "Hey!",
       });
       navigate("/messages", { state: { openChatWith: userData } });
     } catch (err) {
